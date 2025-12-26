8e709318e95422952adf3716a83207b999d93e67
vanyabarny-debug <vanya.barny@gmail.com>
2025-12-26 08:03:12 +0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -3,19 +3,25 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
+// 1. Сразу инициализируем Telegram
+if (window.Telegram && window.Telegram.WebApp) {
+  window.Telegram.WebApp.ready();
+  window.Telegram.WebApp.expand();
+}
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-// Manifest URL required for TON Connect
+// 2. Используй лучше свой домен для манифеста, GitHub часто блочит новые Safari
 const MANIFEST_URL = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
 
 const root = ReactDOM.createRoot(rootElement);
+
+// 3. Убираем StrictMode для теста (он часто причина двойных багов на новых iOS)
 root.render(
-  <React.StrictMode>
-    <TonConnectUIProvider manifestUrl={MANIFEST_URL}>
-      <App />
-    </TonConnectUIProvider>
-  </React.StrictMode>
+  <TonConnectUIProvider manifestUrl={MANIFEST_URL}>
+    <App />
+  </TonConnectUIProvider>
 );
